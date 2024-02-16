a29a1245da7ad320156b05a7992c927ad9097955
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2024-02-16 12:40:22 -0300

    feat: implement python binary search

---
+++ python/binary_search.py
@@ -0,0 +1,12 @@
+def binary_search(nums: list[int], target: int) -> int:
+    begin = 0
+    end = len(nums) - 1
+    while begin <= end:
+        mid = (end - begin) // 2
+        if nums[mid] > target:
+            end = mid - 1
+        elif nums[mid] < target:
+            begin = mid + 1
+        else:
+            return mid
+    return -1
