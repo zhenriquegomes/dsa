e8e7e9fc4df71b961f9645ada7359867ad142d6b
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2024-02-16 18:27:19 -0300

    fix: ajust mid index in binary search

--- python/binary_search.py
+++ python/binary_search.py
@@ -2,7 +2,7 @@ def binary_search(nums: list[int], target: int) -> int:
     begin = 0
     end = len(nums) - 1
     while begin <= end:
-        mid = (end - begin) // 2
+        mid = (begin + end) // 2
         if nums[mid] > target:
             end = mid - 1
         elif nums[mid] < target:
