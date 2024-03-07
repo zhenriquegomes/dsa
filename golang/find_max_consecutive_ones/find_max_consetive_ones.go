68a530d455e234cdce5f2effe5aae726eea87c25
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2024-03-07 06:05:11 -0300

    feat(golang): add find max consecutive ones

---
+++ golang/find_max_consecutive_ones/find_max_consetive_ones.go
@@ -0,0 +1,19 @@
+package findmaxconsecutiveones
+
+func FindMaxConsecutiveOnes(nums []int) int {
+    var (
+		maxConsecutiveOnes int
+		onesCount int
+	)
+    for _, num := range nums {
+		if num == 1 {
+            onesCount++
+			if onesCount > maxConsecutiveOnes {
+				maxConsecutiveOnes = onesCount
+			}
+		} else {
+			onesCount = 0
+		}
+	}
+    return maxConsecutiveOnes
+}
