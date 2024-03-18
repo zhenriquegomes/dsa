fbf70ff014cc5b797928fb942f58fd6139460efe
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2024-03-18 06:48:35 -0300

    feat: add duplicate zeros solution in go

---
+++ golang/duplicate_zeros/duplicate_zeros.go
@@ -0,0 +1,12 @@
+package duplicatezeros
+
+func DuplicateZeros(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == 0 {
+			for j := len(arr) - 1; j > i; j-- {
+				arr[j] = arr[j-1]
+			}
+			i++
+		}
+	}
+}
