ae3624fc8c169831c605ee55ce9c79e9b8ead7d2
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2024-03-19 18:54:16 -0300

    refactor: optimize sortedSquares

--- golang/squares_of_sorted_array/squares_of_sorted_array.go
+++ golang/squares_of_sorted_array/squares_of_sorted_array.go
@@ -1,13 +1,33 @@
 package squaresofsortedarray
 
-import "sort"
+import (
+	"fmt"
+	"math"
+)
 
 func sortedSquares(nums []int) []int {
-	squares := make([]int, len(nums))
-	for i, num := range nums {
-		square := num * num
-		squares[i] = square
+	squares := make([]int, 0, len(nums))
+	l, r := 0, len(nums)-1
+	for l <= r {
+		if math.Abs(float64(nums[l])) > math.Abs(float64(nums[r])) {
+			squares = append(squares, nums[l]*nums[l])
+			l++
+		} else {
+			squares = append(squares, nums[r]*nums[r])
+			r--
+		}
 	}
-	sort.IntSlice(squares).Sort()
+	reverseSlice(squares)
 	return squares
+}
+
+func reverseSlice(nums []int) {
+	fmt.Println(nums)
+	i, j := 0, len(nums)-1
+	for i <= j {
+		nums[i], nums[j] = nums[j], nums[i]
+		i++
+		j--
+	}
+	fmt.Println(nums)
 }
